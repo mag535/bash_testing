c5749093e294778fc1053f9a3311a60b858ef6de
Nu <melisofone@gmail.com>
2020-08-12 12:18:56 -0400

    updating from the local RandFiles folder

---
+++ details.py
@@ -0,0 +1,22 @@
+# This is just to print info on the Python_Files branch
+
+print("In the \'Python_Files\' branch, only python files should be saved.")
+print("There will be other branches for creating, editing, and adding/commiting other file types.")
+
+sym1 = "^"
+sym2 = "@"
+sym3 = " : "
+loop = 47
+
+for i in range(loop):
+    line = ""
+    if i%3 == 0:
+        line += sym1
+    if i%5 == 0:
+        line += sym2
+    else:
+        line += sym3
+    print(line)
+
+print(("sorry, that was some random junk ;)")
+
---
+++ that.py
@@ -0,0 +1,16 @@
+# Some random code to print an ACSII pyramid
+
+pyramid_base = 5
+sym = "*"
+white_space = " - "
+
+for i in range(pyramid_base):
+    line = ""
+    for j in range(i+1):
+        line += sym
+    for k in range(pyramid - (i+1)):
+        line += white_space
+    print(line)
+
+print("It's supposed to be a pyramid")
+
